3d560cdc401457b4ef5344e1a520928e1e27715c
avasilevich <andrey.vasilevich@pressindex.ru>
2018-05-19 16:27:11 +0700

    Initial commit

---
+++ desktop/src/ru/avp/submarine/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.avp.submarine.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.avp.submarine.SubmarineMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SubmarineMain(), config);
+	}
+}
